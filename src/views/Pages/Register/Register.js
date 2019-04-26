5c57e7ae0160d3e825d22c059c140bc7c599f5cf
Cesar <cesarfec7@gmail.com>
2019-04-26 19:12:28 -0300

    alteracao no register

--- src/views/Pages/Register/Register.js
+++ src/views/Pages/Register/Register.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Card, CardBody, CardFooter, Col, Container, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
+import { Button, Card, CardBody, Col, Container, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Row, FormGroup} from 'reactstrap';
 
 class Register extends Component {
   render() {
@@ -11,8 +11,8 @@ class Register extends Component {
               <Card className="mx-4">
                 <CardBody className="p-4">
                   <Form>
-                    <h1>Register</h1>
-                    <p className="text-muted">Create your account</p>
+                    <h1>Registro</h1>
+                    <br/>
                     <InputGroup className="mb-3">
                       <InputGroupAddon addonType="prepend">
                         <InputGroupText>
@@ -33,20 +33,50 @@ class Register extends Component {
                           <i className="icon-lock"></i>
                         </InputGroupText>
                       </InputGroupAddon>
-                      <Input type="password" placeholder="Password" autoComplete="new-password" />
+                      <Input type="password" placeholder="Senha" autoComplete="new-password" />
                     </InputGroup>
-                    <InputGroup className="mb-4">
+                    {/* <InputGroup className="mb-4">
                       <InputGroupAddon addonType="prepend">
                         <InputGroupText>
                           <i className="icon-lock"></i>
                         </InputGroupText>
                       </InputGroupAddon>
                       <Input type="password" placeholder="Repeat password" autoComplete="new-password" />
+                    </InputGroup> */}
+                    <FormGroup>
+                      <Input type="select" name="select" id="select">
+                        <option>CPF</option>
+                        <option>CNPJ</option>
+                      </Input>
+                    </FormGroup>
+                    <InputGroup className="mb-3">
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="icon-user"></i>
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input type="text" placeholder="CPF"/>
                     </InputGroup>
-                    <Button color="success" block>Create Account</Button>
+                    <InputGroup className="mb-3">
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          {/* <i className="icon-user"></i> */}
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input type="text" placeholder="CNPJ"/>
+                    </InputGroup>
+                    <InputGroup className="mb-3">
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          {/* <i className="icon-user"></i> */}
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input type="text" placeholder="Rua"/>
+                    </InputGroup>
+                    <Button color="success" block>Inscreva-se</Button>
                   </Form>
                 </CardBody>
-                <CardFooter className="p-4">
+                {/* <CardFooter className="p-4">
                   <Row>
                     <Col xs="12" sm="6">
                       <Button className="btn-facebook mb-1" block><span>facebook</span></Button>
@@ -55,7 +85,7 @@ class Register extends Component {
                       <Button className="btn-twitter mb-1" block><span>twitter</span></Button>
                     </Col>
                   </Row>
-                </CardFooter>
+                </CardFooter> */}
               </Card>
             </Col>
           </Row>
