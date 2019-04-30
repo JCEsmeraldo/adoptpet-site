52392acd6c6c9723aef672ed73d817c40dc6a393
Cesar <cesarfec7@gmail.com>
2019-04-30 17:19:54 -0300

    alteração register

--- src/views/Pages/Register/Register.js
+++ src/views/Pages/Register/Register.js
@@ -1,79 +1,135 @@
 import React, { Component } from 'react';
-import { Button, Card, CardBody, Col, Container, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Row, FormGroup} from 'reactstrap';
+import {Button, Card, CardBody, Col, Container, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Row, FormGroup} from 'reactstrap';
 
 class Register extends Component {
   render() {
     return (
       <div className="app flex-row align-items-center">
         <Container>
-          <Row className="justify-content-center">
-            <Col md="9" lg="7" xl="6">
-              <Card className="mx-4">
+          {/* <Row form>
+            <Col md="12" lg="12" xl="12"> */}
+              <Card className="mx-2">
                 <CardBody className="p-4">
                   <Form>
                     <h1>Registro</h1>
                     <br/>
-                    <InputGroup className="mb-3">
-                      <InputGroupAddon addonType="prepend">
-                        <InputGroupText>
-                          <i className="icon-user"></i>
-                        </InputGroupText>
-                      </InputGroupAddon>
-                      <Input type="text" placeholder="Nome" autoComplete="nome" />
-                    </InputGroup>
-                    <InputGroup className="mb-3">
-                      <InputGroupAddon addonType="prepend">
-                        <InputGroupText>@</InputGroupText>
-                      </InputGroupAddon>
-                      <Input type="text" placeholder="Email" autoComplete="email" />
-                    </InputGroup>
-                    <InputGroup className="mb-3">
-                      <InputGroupAddon addonType="prepend">
-                        <InputGroupText>
-                          <i className="icon-lock"></i>
-                        </InputGroupText>
-                      </InputGroupAddon>
-                      <Input type="password" placeholder="Senha" autoComplete="new-password" />
-                    </InputGroup>
-                    {/* <InputGroup className="mb-4">
-                      <InputGroupAddon addonType="prepend">
-                        <InputGroupText>
-                          <i className="icon-lock"></i>
-                        </InputGroupText>
-                      </InputGroupAddon>
-                      <Input type="password" placeholder="Repeat password" autoComplete="new-password" />
-                    </InputGroup> */}
-                    <FormGroup>
-                      <Input type="select" name="select" id="select">
-                        <option>CPF</option>
-                        <option>CNPJ</option>
-                      </Input>
-                    </FormGroup>
-                    <InputGroup className="mb-3">
-                      <InputGroupAddon addonType="prepend">
-                        <InputGroupText>
-                          <i className="icon-user"></i>
-                        </InputGroupText>
-                      </InputGroupAddon>
-                      <Input type="text" placeholder="CPF"/>
-                    </InputGroup>
-                    <InputGroup className="mb-3">
-                      <InputGroupAddon addonType="prepend">
-                        <InputGroupText>
-                          {/* <i className="icon-user"></i> */}
-                        </InputGroupText>
-                      </InputGroupAddon>
-                      <Input type="text" placeholder="CNPJ"/>
-                    </InputGroup>
-                    <InputGroup className="mb-3">
-                      <InputGroupAddon addonType="prepend">
-                        <InputGroupText>
-                          {/* <i className="icon-user"></i> */}
-                        </InputGroupText>
-                      </InputGroupAddon>
-                      <Input type="text" placeholder="Rua"/>
-                    </InputGroup>
-                    <Button color="success" block>Inscreva-se</Button>
+                    <Row>
+                      <Col md={12} lg={4} xl={4}>
+                        <InputGroup className="mb-3">
+                          <InputGroupAddon addonType="prepend">
+                            <InputGroupText>
+                              <i className="icon-user"></i>
+                            </InputGroupText>
+                          </InputGroupAddon>
+                          <Input type="text" placeholder="Nome" autoComplete="nome" />
+                        </InputGroup>
+                      </Col>
+                      <Col md={12} lg={4} xl={4}>
+                        <InputGroup className="mb-3">
+                          <InputGroupAddon addonType="prepend">
+                            <InputGroupText>@</InputGroupText>
+                          </InputGroupAddon>
+                          <Input type="text" placeholder="Email" autoComplete="email" />
+                        </InputGroup>
+                      </Col>
+                      <Col md={12} lg={4} xl={{size: 4}}>
+                        <InputGroup className="mb-3">
+                          <InputGroupAddon addonType="prepend">
+                            <InputGroupText>
+                              <i className="icon-lock"></i>
+                            </InputGroupText>
+                          </InputGroupAddon>
+                          <Input type="password" placeholder="Senha" autoComplete="new-password" />
+                        </InputGroup>
+                      </Col>
+                    </Row>
+                    <Row>
+                    <Col md={12} lg={2} xl={2}>
+                      <FormGroup>
+                        <Input type="select" name="select" id="select">
+                          <option>CPF</option>
+                          <option>CNPJ</option>
+                        </Input>
+                      </FormGroup>
+                    </Col>
+                    <Col md={12} lg={6} xl={5}>
+                      <InputGroup className="mb-3">
+                        <InputGroupAddon addonType="prepend">
+                        </InputGroupAddon>
+                        <Input type="text" placeholder="CPF"/>
+                      </InputGroup>
+                    </Col>
+                    {/* <Col md={12} lg={6} xl={5}>
+                      <InputGroup className="mb-3">
+                        <InputGroupAddon addonType="prepend">
+                        </InputGroupAddon>
+                        <Input type="text" placeholder="CNPJ"/>
+                      </InputGroup>
+                    </Col> */}
+                    </Row>
+                    <Row>
+                      <Col md={12} lg={3} xl={5}>
+                        <InputGroup className="mb-3">
+                          <InputGroupAddon addonType="prepend">
+                          </InputGroupAddon>
+                          <Input type="text" placeholder="Rua"/>
+                        </InputGroup>
+                      </Col>
+                      <Col md={12} lg={3} xl={3}>
+                        <InputGroup className="mb-3">
+                          <InputGroupAddon addonType="prepend">
+                          </InputGroupAddon>
+                          <Input type="text" placeholder="Bairro"/>
+                        </InputGroup>
+                      </Col>
+                      <Col md={12} lg={2} xl={2}>
+                        <InputGroup className="mb-3">
+                          <InputGroupAddon addonType="prepend">
+                          </InputGroupAddon>
+                          <Input type="text" placeholder="Numero"/>
+                        </InputGroup>
+                      </Col>
+                      <Col md={12} lg={2} xl={2}>
+                        <InputGroup className="mb-3">
+                          <InputGroupAddon addonType="prepend">
+                          </InputGroupAddon>
+                          <Input type="text" placeholder="Complemento"/>
+                        </InputGroup>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col md={12} lg={2} xl={3}>
+                        <InputGroup className="mb-3">
+                          <InputGroupAddon addonType="prepend">
+                          </InputGroupAddon>
+                          <Input type="text" placeholder="Cidade"/>
+                        </InputGroup>
+                      </Col>
+                      <Col md={12} lg={2} xl={3}>
+                        <InputGroup className="mb-3">
+                          <InputGroupAddon addonType="prepend">
+                          </InputGroupAddon>
+                          <Input type="text" placeholder="Estado"/>
+                        </InputGroup>
+                      </Col>
+                      <Col md={12} lg={2} xl={3}>
+                        <InputGroup className="mb-3">
+                          <InputGroupAddon addonType="prepend">
+                          </InputGroupAddon>
+                          <Input type="text" placeholder="País"/>
+                        </InputGroup>
+                      </Col>
+                      <Col md={12} lg={2} xl={3}>
+                        <InputGroup className="mb-3">
+                          <InputGroupAddon addonType="prepend">
+                          </InputGroupAddon>
+                          <Input type="text" placeholder="Telefone"/>
+                        </InputGroup>
+                      </Col>
+                    </Row>
+                    <Col md={12} lg={2} xl={{size: 4, offset: 4}}>
+                      <Button color="success" block>Inscreva-se</Button>
+                    </Col>
                   </Form>
                 </CardBody>
                 {/* <CardFooter className="p-4">
@@ -87,8 +143,8 @@ class Register extends Component {
                   </Row>
                 </CardFooter> */}
               </Card>
-            </Col>
-          </Row>
+            {/* </Col>
+          </Row > */}
         </Container>
       </div>
     );
